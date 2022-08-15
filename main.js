d25b18b58dc47f21830bb77dcb09566d8fc5660e
andrejt0807 <andrejj.tokic@gmail.com>
2022-08-15 23:22:42 +0200

    First Commit

---
+++ main.js
@@ -0,0 +1,137 @@
+var name;
+var names = [];
+var names2;
+var userTR = document.getElementById("nameTR");
+
+document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    Create();
+    Read();
+    document.getElementById("form").reset();
+});
+
+function Create() {
+    let storage = JSON.parse(localStorage.getItem("names"));
+    name = document.getElementById("name").value;
+    if (name == "") {
+        alert("Unesi podatke")
+    } else {
+        if (storage == null) {
+            names.push(name);
+            localStorage.setItem("names", JSON.stringify(names))
+        } else {
+            names = JSON.parse(localStorage.getItem("names"));
+            names.push(name);
+            localStorage.setItem("names", JSON.stringify(names))
+
+        }
+    }
+}
+
+function Read() {
+    userTR.innerHTML = "";
+    names2 = JSON.parse(localStorage.getItem("names"));
+    if (names2 != null) {
+        for (var i = 0; i < names2.length; i++) {
+            userTR.innerHTML += `
+            <div class="bg-dark border border-success text-white card mb-2">
+            <div class="card-body">
+                <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>
+                ${names2[i]}
+                </p>
+                <button class="col-5 text-white btn btn-warning" onclick="Update(${i})">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                </svg>  Uredi          
+                </button>
+                <button class="col-5 text-white btn btn-danger" onclick="Delete(${i})">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            </svg>  Obriši          
+                </button>
+            </div> 
+            </div>
+            `
+        }
+    }
+}
+function Update(i3) {
+    let names4 = JSON.parse(localStorage.getItem("names"));
+    userTR.innerHTML = "";
+    for (var i = 0; i < names4.length; i++) {
+        if (i == i3) {
+            userTR.innerHTML +=
+                `
+            <div class="bg-dark border border-danger text-white card mb-2">
+            <div class="card-body">
+                <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>
+                Korisnik:                 
+                </p>
+                <input class="mb-2 form-control" id="newName" placeholder="${names4[i]}">
+                <button class="col-5 text-white btn btn-success" onclick="Update2(${i})">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                </svg>  Ažuriraj          
+                </button>
+                <button class="col-5 text-white btn btn-danger" onclick="Read()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>  Prekini          
+                </button>
+            </div> 
+            </div>
+            `
+        } else {
+            userTR.innerHTML +=
+                `
+            <div class="bg-dark border border-success text-white card mb-2">
+            <div class="card-body">
+                <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                </svg>
+                Korisnik: ${names2[i]}
+                </p>
+                <button disabled class="col-5 text-white btn btn-warning" onclick="Update(${i})">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                </svg>  Uredi          
+                </button>
+                <button disabled class="col-5 text-white btn btn-danger" onclick="Delete(${i})">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            </svg>  Obriši          
+                </button>
+            </div> 
+            </div>
+        `
+        }
+    }
+}
+function Update2(i) {
+    let names5 = JSON.parse(localStorage.getItem("names"));
+    names5[i] = document.getElementById("newName").value;
+    if (names5[i] == "") {
+        alert("Unesi podatke");
+    } else {
+        localStorage.setItem("names", JSON.stringify(names5));
+        Read();
+    }
+}
+function Delete(i2) {
+    let names3 = JSON.parse(localStorage.getItem("names"));
+    names3.splice(i2, 1);
+    localStorage.setItem("names", JSON.stringify(names3));
+    Read();
+}
+
+
